e4c2f563a6fb1b491dedc91992254e467b13a96f
Ian Hom <ianhom@163.com>
2016-08-22 17:59:06 +0800

    Update accumulation.c

--- accumulation.c
+++ accumulation.c
@@ -2,14 +2,31 @@ typedef unsigned int uint32;
 
 uint32 test1(uint32 a, uint32 b)
 {
-    int i = a;
+    uint32 i = a;
+    while((++i) <= b) a += i+1;
+    return a;
+}
+
+uint32 test2(uint32 a, uint32 b)
+{
+    uint32 i;
+    for(i = a; i <= b; i++)
+    {
+        a += i+1;
+    }
+    return a;
+}
+
+uint32 test3(uint32 a, uint32 b)
+{
+    uint32 i = a;
 __loop:
     a += i+1;
     if((++i) == b)    return a;
     goto __loop;
 }
 
-uint32 test2(uint32 a, uint32 b)
+uint32 test4(uint32 a, uint32 b)
 {
     return ((b*b - a*a + a + b) >> 1);
 }
@@ -23,12 +40,12 @@ uint32 fn(uint32 s)
     return 0;
 }
 
-uint test3 (uint32 a, uint32 b)
+uint test5 (uint32 a, uint32 b)
 {
     return (fn(b) - fn(a) + a);
 }
 
-uint32 test5(uint32 a, uint32 b)
+uint32 test6(uint32 a, uint32 b)
 {
     static uint32 c = 0;
     c += b;
@@ -38,5 +55,5 @@ uint32 test5(uint32 a, uint32 b)
         c = 0;
         return a;
     }
-    return test1(a,b);
+    return test6(a,b);
 }
