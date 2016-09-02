daa62d3661bc9b87b6ca21594c05113babe67dd2
Ian Hom <ianhom@163.com>
2016-09-02 23:10:17 +0800

    Update ReturnNoValue.c

--- ReturnNoValue.c
+++ ReturnNoValue.c
@@ -2,18 +2,18 @@
 
 int test2()
 {
-    return 5;
+    return 5; /* Normal return */
 }
 
 int test1()
 {
-    return;
+    return;   /* Should return an int value */
 }
 
 void main()
 {
     int a = 10;
-    test2();
-    a = test1();
-    printf("%d\n",a);
+    test2();            /* Call test2 fisrt */
+    a = test1();        /* Then call test1  */
+    printf("%d\n",a);   /* "a" may be 5 */
 }
