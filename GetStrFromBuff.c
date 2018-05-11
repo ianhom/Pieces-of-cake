d4733fd198cae6d267f0b83701f664d7f1644bc0
A Byte of China <ianhom@163.com>
2018-05-12 06:30:12 +0800

    Update GetStrFromBuff.c

--- GetStrFromBuff.c
+++ GetStrFromBuff.c
@@ -20,10 +20,7 @@ char* GetStrFromBuff(char *pu8Buff, unsigned int u32Head, unsigned int u32Size)
     while(1)
     {
         au8Res[u32Index] = puc8Buff[(Head+unIndex)%u32Size];
-        if((au8Res[u32Index] == '\r')
-          &&(puc8Buff[(Head+unIndex+1)%u32Size] == '\n')
-          &&(puc8Buff[(Head+unIndex+2)%u32Size] == '\r')
-         && (puc8Buff[(Head+unIndex+3)%u32Size] == '\n'))
+        if((au8Res[u32Index] == '\r')&&(puc8Buff[(Head+unIndex+1)%u32Size] == '\n')&&（0 != u32Index）)
         {
             break;
         }
