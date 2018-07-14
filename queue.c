a51cb5713e106d9e76271bde3ec77f24903ddd10
A Byte of China <ianhom@163.com>
2018-07-14 10:46:45 +0800

    Add Queue_Deinit funciton
    Add `Queue_Deinit` function, plus some protection of max count of queues

--- queue.c
+++ queue.c
@@ -8,6 +8,8 @@ typedef struct _T_QUEUE
     uint8  au8Data[1];
 }T_QUEUE;
 
+#define MAX_NUM_QUEUE                 (0xFFFF)
+
 volatile static uint16 sg_u16QueueCnt = 0;
 
 T_QUEUE* Queue_Init(uint8 u8MaxElm, uint16 u16Len)
@@ -15,13 +17,30 @@ T_QUEUE* Queue_Init(uint8 u8MaxElm, uint16 u16Len)
     T_QUEUE *ptQueue = (T_QUEUE*)malloc(u8MaxElm*u16Len+sizeof(T_QUEUE));
     if(NULL == ptQueue)
     {
-        return SW_ERROR
+        return SW_ERROR;
     }
     ptQueue->u8MaxElm = u8MaxElm;
     ptQueue->u8Head   = 0;
     ptQueue->u8Tail   = 0;
     ptQueue->u8Cnt    = 0;
     pTQueue->u16Len   = u16Len;
-    sg_u16QueueCnt++;
+    if(MAX_NUM_QUEUE != sg_u16QueueCnt)
+    {
+        sg_u16QueueCnt++;
+    }
     return ptQUeue;
+}
+
+BYTE Queue_Deinit(T_QUEUE* ptQueue)
+{
+    if(NULL == ptQueue)
+    {
+        return SW_ERROR;
+    }
+    free(ptQueue);
+    if(sg_u16QueueCnt)
+    {    
+        sg_u16QueueCnt--;
+    }
+    return SW_OK;
 }
