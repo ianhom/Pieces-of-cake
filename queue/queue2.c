0483e5b27affd243e7ec550426600f643439c99d
A Byte of China <ianhom@163.com>
2018-08-04 17:28:32 +0800

    Update queue2.c

--- queue/queue2.c
+++ queue/queue2.c
@@ -1,13 +1,3 @@
-typedef struct __T_Q
-{
-    uint8  u8Ch;
-    uint8  u8Head;
-    uint8  u8Tail;
-    uint8  u8MaxElm;
-    uint8  u8Len;
-    uint8  u8Cnt;
-    uint8 *pu8Data;
-}T_Q;
 
 static T_Q sg_aptQ[MAX_NUM_Q] = {NULL};
 
