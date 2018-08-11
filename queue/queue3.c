67728a85d8b514fc5e9de4a047b15a955540b315
A Byte of China <ianhom@163.com>
2018-08-11 22:36:14 +0800

    Update queue3.c

--- queue/queue3.c
+++ queue/queue3.c
@@ -1,20 +1,5 @@
 /* We are going to create a queue module which can save length information */
 
-typedef struct _T_Q
-{
-    uint16  u16Ch;
-    uint16  u16Head;
-    uint16  u16Tail;
-    uint16  u16MaxElm;
-    uint16  u16Len;
-    uint16  u16Cnt;
-    uint8  *pu8Data;
-    uint8  *pu8Len;
-}T_Q;
-
-#ifndef MAX_NUM_Q
-#define MAX_NUM_Q        (10)
-#endif
 
 static T_Q sg_atQ[MAX_NUM_Q] = {0}; 
 static uint16 sg_u16Ch = 0;
