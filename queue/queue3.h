c7c3ea4c0b9d743cc157da9f06bba90b24290974
A Byte of China <ianhom@163.com>
2018-08-15 00:13:59 +0800

    Update queue3.h

--- queue/queue3.h
+++ queue/queue3.h
@@ -1,3 +1,12 @@
+/******************************************************************************
+* File       : queue.h
+* Function   : General queue operation with length information    
+* Version    : V1.00
+* Author     : Ian
+* Date       : 13th Aug 2018
+* History    :  No.  When           Who           What
+*               1    13/Aug/2018    Ian           Create
+******************************************************************************/
 
 typedef struct _T_Q
 {
@@ -14,3 +23,60 @@ typedef struct _T_Q
 #ifndef MAX_NUM_Q
 #define MAX_NUM_Q        (10)
 #endif
+
+/******************************************************************************
+* Name       : WORD16 Q_Init(uint16 u16MaxElm, uint16 u16Len)
+* Function   : Init a new queue, save the key information of queue and return the
+*              channel number if succeed.
+* Input      : uint16 u16MaxElm   1~65535   The maximum of elements in the queue
+*              uint16 u16Len      1~65535   The length of each element in byte
+* Output:    : None
+* Return     : 1~65535   Successful operation & queue channel number.
+*            : SW_ERROR  Failed operation
+* Version    : V1.00
+* Author     : Ian
+* Date       : 13th Aug 2018
+******************************************************************************/
+WORD16 Q_Init(uint16 u16MaxElm, uint16 u16Len);
+
+/******************************************************************************
+* Name       : WORD32 Q_En(uint16 u16Ch, uint16 u16Len, uint8 *pu8Data)
+* Function   : Enqueue operation
+* Input      : uint16 u16Ch       1~65535   The channel number of queue
+*              uint16 u16Len      1~65535   The length of each element in byte
+*              uint8 *pu8Data               The data pointer to be saved
+* Output:    : None
+* Return     : SW_OK     Successful operation
+*            : SW_ERROR  Failed operation
+* Version    : V1.00
+* Author     : Ian
+* Date       : 13th Aug 2018
+******************************************************************************/
+WORD32 Q_En(uint16 u16Ch, uint16 u16Len, uint8 *pu8Data);
+
+/******************************************************************************
+* Name       : WORD32 Q_De(uint16 u16Ch, uint16 *pu16Len, uint8 *pu8Data)
+* Function   : Dequeue operation
+* Input      : uint16 u16Ch       1~65535   The channel number of queue
+*              uint16 u16Len      1~65535   The length of each element in byte
+* Output:    : uint8 *pu8Data               The data pointer to be loaded
+* Return     : SW_OK     Successful operation
+*            : SW_ERROR  Failed operation
+* Version    : V1.00
+* Author     : Ian
+* Date       : 13th Aug 2018
+******************************************************************************/
+WORD32 Q_De(uint16 u16Ch, uint16 *pu16Len, uint8 *pu8Data);
+
+/******************************************************************************
+* Name       : uint16 Q_Cnt(uint16 u16Ch)
+* Function   : Return the count of current element of queue.
+* Input      : uint16 u16Ch       1~65535   The channel number of queue
+* Output:    : None
+* Return     : 1~65535   Successful operation & the count of the element
+*            : SW_ERROR  Failed operation or NONE element
+* Version    : V1.00
+* Author     : Ian
+* Date       : 13th Aug 2018
+******************************************************************************/
+uint16 Q_Cnt(uint16 u16Ch);
