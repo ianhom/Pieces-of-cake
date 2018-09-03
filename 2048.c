133dbad8af50b38344ba6c3efa6ec241eead5a7b
A Byte of China <ianhom@163.com>
2018-09-03 20:29:17 +0800

    Update 2048.c

--- 2048.c
+++ 2048.c
@@ -1,6 +1,7 @@
 int UpdatData(unsigned char *arr)
 {
     int i,m,next;
+    /* Try to find a NON-0 number for next block */
     for(i = 0; i < 4; i++)
     {
         next =-1;
@@ -13,14 +14,16 @@ int UpdatData(unsigned char *arr)
             }
         }
         
+        /* If we find one */
         if(-1 != next)
-        {
+        {   /* If the current one is 0 */
             if(0 == arr[i])
-            {
+            {   /* Switch the number with next */
                 arr[i] = arr[next];
                 arr[next] = 0;
                 i = i - 1;
             }
+            /* If the current one and next one are the same */
             else if(arr[i] == arr[next])
             {
                 arr[i] = arr[i]*2;
