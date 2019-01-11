72cfeb5795f1cf5347fdbbc0e38f3712930590d7
A Byte of China <ianhom@163.com>
2019-01-11 21:29:16 +0800

    Update main_arg.c

--- main_arg.c
+++ main_arg.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(int argc, char **argv)
 {
+    printf("The first parameter is the Path and name of this program \"%s\"\n",argv[0]);
     for(int i = 0; i < argc; i++)
     {
         printf("The No.%d parameter is %s\n",i,argv[i]);
