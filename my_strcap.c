a25f459b876bf7816d1b32832fafd366f735aac0
A Byte of China <ianhom@163.com>
2019-01-14 21:10:50 +0800

    Update my_strcap.c

--- my_strcap.c
+++ my_strcap.c
@@ -1,4 +1,17 @@
 char* my_strcap(char *str1, char *str2)
 {
-
+    if((NULL == str1)||(NULL == str2))
+    {
+        return NULL;
+    }
+    int len,i;
+    for(len = 0; str1[len]!='\0';len++);
+    for(i = 0; i < len; i++)
+    {
+        if(str1[i] != str2[i])
+        {
+            return NULL;
+        }
+    }
+    return str1;
 }
