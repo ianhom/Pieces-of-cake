b6fe97cc5b70d786287df6e4711e7c94f41dfe32
A Byte of China <ianhom@163.com>
2019-01-18 20:32:28 +0800

    Update my_strlen.c

--- mylib/my_strlen.c
+++ mylib/my_strlen.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+
+#define MY_STRLEN(s,l) do{if(NULL!=s)for(l=0;s[l]!='\0';l++);else l=0;}while(0)
+
 int my_strlen(char *s)
 {
     if(NULL == s)
@@ -9,4 +13,8 @@ int my_strlen(char *s)
     return len;
 }
 
-#define MY_STRLEN(s,l) do{if(NULL!=s)for(l=0;s[l]!='\0';l++);else l=0;}while(0)
+void main(int argc, char **argv)
+{
+    printf("The length of the \"%s\" is %d\n",argv[1], my_strlen(argv[1]));
+    return;
+}
