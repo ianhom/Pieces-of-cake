cf2f0bcfde1b8ca6e9a59863aa256367703b4427
A Byte of China <ianhom@163.com>
2019-01-19 09:23:57 +0800

    Update my_strcpy.c

--- mylib/my_strcpy.c
+++ mylib/my_strcpy.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #define MY_STRCPY(DES,SRC) do{char *des=DES,*src=SRC;if((NULL!=des)&&(NULL!=src))while((*des++=*src++)!='\0');}while(0)
 
 char* my_strcpy(char *str1, char *str2)
@@ -9,4 +11,10 @@ char* my_strcpy(char *str1, char *str2)
     char *t=str1;
     while((*t++=*str2++)!='\0');
     return str1;
+}
+
+void main(int argc, char **argc)
+{
+    char a[100]={0};
+    printf("The copied string is %s\n",my_strcpy(a,argv[1]));
 }
