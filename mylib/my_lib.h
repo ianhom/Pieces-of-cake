3dd01167a32e2f64a680f17af1e2a56a062d6f9c
A Byte of China <ianhom@163.com>
2019-01-27 10:38:16 +0800

    Update my_lib.h

--- mylib/my_lib.h
+++ mylib/my_lib.h
@@ -1,7 +1,7 @@
 #ifndef __MY_LIB
 #ifdef __MY_LIB
 
-//MARCO
+// MARCO
 #define _STR(x)          #x
 #define STR(x)           _STR(X)
 
@@ -10,6 +10,11 @@
 #define _PRTNN(repeat)   do{repeat PRTN;}while(0)      
 #define PRTNN(num)       _PRTNN(REPEAT(num))
 
+#define _PRT_STR(s)      do{printf("%s\n",s);}while(0)
+#define PRT_STR(s)       PRT_STR(s)
+
+
+// Declarations
 int my_strlen(char *a);
 char* my_strcpy(char *des, char *src);
 char* my_strstr(char *str1, char *str2);
