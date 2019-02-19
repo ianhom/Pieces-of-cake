c81956d8d501c00f270b5d7bb2ec642c01e8c5ab
A Byte of China <ianhom@163.com>
2019-02-20 07:19:32 +0800

    Update my_atoi.c

--- mylib/my_atoi.c
+++ mylib/my_atoi.c
@@ -3,9 +3,9 @@
 int my_atoi(char *s)
 {
     int sig=1,ret=0;
-    if(0==s)return 0;
-    while(*s==' ')s++;
-    if(*s=='-'||*s=='+')sig=','-*s++;
-    while(*s>='0'&&*s<='9')ret=ret*10+*s++-'0';
+    if(0==s)return 0;                           // check empty pointer
+    while(*s==' ')s++;                          // check space
+    if(*s=='-'||*s=='+')sig=','-*s++;           // check signature
+    while(*s>='0'&&*s<='9')ret=ret*10+*s++-'0'; // converting
     return sig*ret;
 }
