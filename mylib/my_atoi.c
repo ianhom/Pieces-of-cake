8e7fe6628d6cfc8518fc3dddc28601db94866d2a
A Byte of China <ianhom@163.com>
2019-02-19 20:47:49 +0800

    Update my_atoi.c

--- mylib/my_atoi.c
+++ mylib/my_atoi.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 
-#define my_strlen(s,l)  do{l=0;if(NULL!=s)for(;s[l]!='\0';l++);}while(0)
-
 int my_atoi(char *s)
 {
-    int ret=0;
-    int l,f;
-    
-    my_strlen(s,l);
-    for(int i=0;i<l;i++)
-    {
-        f=1;
-        for(int j=0;j<l-1-i;j++)
-            f*=10;
-        ret+=(s[i]-'0')*f;
-    }
-    return ret;
+    int sig=1,ret=0;
+    if(0==s)return 0;
+    while(*s==' ')s++;
+    if(*s=='-'||*s=='+')sig=','-*s++;
+    while(*s>='0'&&*s<='9')ret=ret*10+*s++-'0';
+    return sig*ret;
 }
