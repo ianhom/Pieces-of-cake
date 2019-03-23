03ecd15decb28f1714208c380578f72bddba61c0
A Byte of China <ianhom@163.com>
2019-03-23 17:55:25 +0800

    Update array_linkedlist.c

--- linklist/array_linkedlist.c
+++ linklist/array_linkedlist.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
 
-int a[100]={0};
+#define MAX  100
 
+typedef struct _L
+{
+    int data;
+    int next;
+    int prev;
+}Node;
+
+Node a[MAX]={0};
+
+void init(void)
+{
+    for(int i=0;i<MAX;i++)
+    {
+        a[i].data=0;
+        a[i].next=0;
+        a[i].prev=0;
+    }
+}
