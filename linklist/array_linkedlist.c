357cf322bb165b0dbb587381625e7f7694a047b4
A Byte of China <ianhom@163.com>
2019-03-30 09:57:19 +0800

    Update array_linkedlist.c

--- linklist/array_linkedlist.c
+++ linklist/array_linkedlist.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 #define MAX  100
-static int sg_cnt=1;  //start with 1,and the first(0) node will Not used
-static int index=1；
+static int sg_cnt=0;  
+static int index=1;//start with 1,and the first(0) node will Not used
 static int head=0;
 static int tail=0;
 typedef struct _L
@@ -29,12 +29,22 @@ int add(int data)
 {
     if(sg_cnt<MAX)
     {
-        a[sg_cnt].data=data;
-        s[sg_cnt].prex=sg_cnt-1;
-        a[sg_cnt].next=sg_cnt+1;
+        a[index].data=data;
+        s[index].prex=sg_cnt-1;
+        a[index].next=sg_cnt+1;
+        index++;
         sg_cnt++;
     }
     else//last one is used
+    {
+        for(int i=0;i<MAX;i++)
+        {
+            if(0==a[i].prev&&0==a[i].next)
+            {
+            //
+            }
+        }
+    }
         
 }
     
