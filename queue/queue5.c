6e8bc1ea73b0cb9f9ced400f44a8e2ea0c89c6dd
A Byte of China <ianhom@163.com>
2020-02-12 07:10:10 +0800

    Update queue5.c

--- queue/queue5.c
+++ queue/queue5.c
@@ -4,8 +4,8 @@
 #if(MARCO_QUEUE)
 typedef struct _q{int h,t,c,s,*d[0];}Q;
 #define Q_INIT(q,n)    do{if(q=(Q*)malloc(sizeof(Q)+sizeof(int*)*n){q->h=q->t=q->c=0;q->s=n;}}while(0)
-#define Q_IN(q,n)      do{if(q->c!=q->s){q->d[q->t]=n;q->t=(++q->t)%q->s;q->c++;}}while(0)
-#define Q_OUT(q,t)     do{if(q->c!=0){t=q->d[q->h];q->h=(++q->h)%q->s;q->c--;}}while(0)
+#define Q_IN(q,n)      do{if(q->c!=q->s){q->d[q->t]=(int*)n;q->t=(++q->t)%q->s;q->c++;}}while(0)
+#define Q_OUT(q,n)     do{if(q->c!=0){n=q->d[q->h];q->h=(++q->h)%q->s;q->c--;}}while(0)
 #define Q_CNT(q)       (q->c)
 #define Q_FULL(q)      (q->c == q->s)
 #define Q_EMPTY(q)     (q->c == 0)
