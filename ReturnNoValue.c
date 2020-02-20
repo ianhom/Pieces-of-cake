ab3aa51a30db607b5dbe823451741081d45720f3
A Byte of China <ianhom@163.com>
2020-02-21 07:52:54 +0800

    Update ReturnNoValue.c

--- ReturnNoValue.c
+++ ReturnNoValue.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-int test2()
+int test2(void)
 {
     return 5; /* Normal return */
 }
 
-int test1()
+int test1(void)
 {
     return;   /* Should return an int value */
 }
 
-void main()
+void main(void)
 {
     int a = 10;
     test2();            /* Call test2 fisrt */
     a = test1();        /* Then call test1  */
     printf("%d\n",a);   /* "a" is 5 */
     a = test1();
-    printf("%d\n",a);   /* "a" is 2 */
+    printf("%d\n",a);   /* "a" may be 2 */
 }
