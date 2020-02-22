7e072e131c0adb0651316e35c3da2a0977c99a8d
A Byte of China <ianhom@163.com>
2020-02-22 16:40:24 +0800

    Update queue7.c

--- queue/queue7.c
+++ queue/queue7.c
@@ -5,6 +5,9 @@ typedef struct _q{int h,t,c,s,*d[0];}Q;
 #define INIT(n)       ({Q *_q;if(_q=(Q*)malloc(sizeof(Q)+sizeof(int*)*n)){_q->h=_q->t=_q->c=0;_q->s=n;}_q;})
 #define IN(q,n)       ({int _ret=0;if(q->c!=q->s){q->d[q->t]=(int*)n;q->t=(++q->t)%q->s;q->c++;_ret=1;}_ret;})
 #define OUT(q)        ({int *_ret=0;if(q->c!=0){_ret=q->d[q->h];q->h=(++q->h)%q->s;q->c--;}_ret;})
+#define CNT(q)        (q->c)
+#define EMPTY(q)      (q->c==0)
+#define FULL(q)       (q->c==q->s)
 
 // Example
 void main(int argc, char **argv)
