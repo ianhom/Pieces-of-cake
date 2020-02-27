f340a286a0b490c26d8112d4d41b36d0aaac42a9
A Byte of China <ianhom@163.com>
2020-02-27 21:16:47 +0800

    Update my_container_of.c

--- mylib/my_container_of.c
+++ mylib/my_container_of.c
@@ -1,2 +1,15 @@
 #define container_of(p,t,m) ({const typeof(((t*)0)->m) *_p=(p);(t*)((char*)_p-((size_t)&((t*)0)->m));})
 #define CTNR_OF(p,t,m) ((t*)((char*)p-((size_t)&((t*)0)->m)))
+
+//Example
+typedef struct _t{
+    int a;
+    char b;
+    int c;
+}T;
+
+void main(void)
+{
+    T t;
+    printf("The container address is %p\n",container_of(&t.b,T,b));
+}
