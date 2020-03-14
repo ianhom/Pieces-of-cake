8d7c951720004248dca13e4b483ff35d69c06bd4
A Byte of China <ianhom@163.com>
2020-03-14 14:12:17 +0800

    Update hell_world.c

--- hell_world.c
+++ hell_world.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
+static cs="Hell ";
 int puts(const char *s)
 {
-    return 0;
+    int len = hlen = 0;
+    int flg = 0;
+    while(*s!='\0')
+    {
+        if(*s==cs[hlen])
+        {
+            if(hlen<4)
+                hlen++;
+            else
+            {
+                putchar('o');
+                len++;
+                hlen=0;
+            }
+        }
+        else
+            hlen=0;
+        putchar(*s);
+        len++;
+        s++;     
+    }
+    putchar('\n');
+    return len;
 }
 
 void main(void)
 {
-    printf("Hello world\n");
+    printf("Hell world\n");
 }
